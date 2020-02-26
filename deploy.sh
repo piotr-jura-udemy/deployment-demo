ea528e1cab67d286b40f1047c62755f79530247d
Piotr Jura <>
2020-02-26 19:02:59 +0100

    5th

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 cd /var/www/deployment-demo/repo
 git fetch --tags
-TAG=$(git tag | sort -V | tail -n)
+TAG=$(git tag | sort -V | tail -n 1)
 
 cd /var/www/deployment-demo
 git clone --branch $TAG https://github.com/piotr-jura-udemy/deployment-demo.git ${TAG}
